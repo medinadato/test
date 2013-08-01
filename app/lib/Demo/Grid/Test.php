222e4c3bdbd7627f1e05ff8cae296cee19a5e438
Renato Medina <medinadato@gmail.com>
2013-08-01 17:18:50 +1000

    More data for the mgrid

--- app/lib/Demo/Grid/Test.php
+++ app/lib/Demo/Grid/Test.php
@@ -22,10 +22,21 @@ class Test extends \Mgrid\Grid
                 ->addColumn(array(
                     'label' => 'Cod.',
                     'index' => 'id',
+                    'filter' => array(
+                        'render' => array(
+                            'type' => 'number',
+                            'range' => 1,
+                        ),
+                    ),
                 ))
                 ->addColumn(array(
                     'label' => 'Company',
                     'index' => 'companyName',
+                    'filter' => array(
+                        'render' => array(
+                            'type' => 'text',
+                        ),
+                    ),
                 ))
                 ->addColumn(array(
                     'label' => 'Role',
@@ -53,6 +64,12 @@ class Test extends \Mgrid\Grid
                     'render' => array(
                         'type' => 'money',
                     ),
+                    'filter' => array(
+                        'render' => array(
+                            'type' => 'number',
+                            'range' => 1,
+                        ),
+                    ),
                 ))
                 ->addColumn(array(
                     'label' => 'Username',
@@ -61,10 +78,13 @@ class Test extends \Mgrid\Grid
                 ->addColumn(array(
                     'label' => 'Birthday',
                     'index' => 'birthday',
-                    'filter' => array(
-                        'render' => 'date',
-                    ),
                     'render' => 'date',
+                    'filter' => array(
+                        'render' => array(
+                            'type' => 'date',
+                            'range' => 1,
+                        )
+                    ),
                 ))
                 ->addColumn(array(
                     'label' => 'Last access',
@@ -72,7 +92,15 @@ class Test extends \Mgrid\Grid
                 ))
                 ->addColumn(array(
                     'label' => 'Status',
-                    'index' => 'companyName',
+                    'index' => 'statusId',
+                    'filter' => array(
+                        'render' => array(
+                            'type' => 'select',
+                            'attributes' => array(
+                                'multiOptions' => array(1 => 'Enable', 2 => 'Disabled'),
+                            ),
+                        ),
+                    ),
 //                    'condition' => function ($row) {
 //                        return ($row['statusId'] != 1);
 //                    },
@@ -142,6 +170,50 @@ class Test extends \Mgrid\Grid
                 'roleName' => 'Employee',
                 'personName' => 'Anacleto',
                 'username' => 'anacleto@mdnsolutions.com',
+                'birthday' => '1991-09-01',
+                'lastAccess' => '2001-09-11 09:18:25',
+                'balance' => '4263',
+                'statusId' => 1,
+            ),
+            9 => array(
+                'id' => 11,
+                'companyName' => 'Baconzitos',
+                'roleName' => 'Counter',
+                'personName' => 'Rodrigo Medina',
+                'username' => 'johndue@mdnsolutions.com',
+                'birthday' => '1986-01-03',
+                'lastAccess' => '2013-01-05 00:15:13',
+                'balance' => '158.12',
+                'statusId' => 1,
+            ),
+            12 => array(
+                'id' => 12,
+                'companyName' => 'Cheettos',
+                'roleName' => 'Role 02',
+                'personName' => 'Mary Doe',
+                'username' => 'marydue@mdnsolutions.com',
+                'birthday' => '1955-06-10',
+                'lastAccess' => '2012-08-09 12:15:13',
+                'balance' => '82458.12',
+                'statusId' => 2,
+            ),
+            17 => array(
+                'id' => 118,
+                'companyName' => 'Coca Cola',
+                'roleName' => 'Empresario',
+                'personName' => 'Marcelo Raposo',
+                'username' => 'raposo@mdnsolutions.com',
+                'birthday' => '1968-09-11',
+                'lastAccess' => '2013-03-09 14:15:13',
+                'balance' => '9655.14',
+                'statusId' => 1,
+            ),
+            18 => array(
+                'id' => 125,
+                'companyName' => 'Hardware Store',
+                'roleName' => 'Employee',
+                'personName' => 'Lourival',
+                'username' => 'lourival@mdnsolutions.com',
                 'birthday' => '1991-09-01',
                 'lastAccess' => '2001-09-11 09:18:25',
                 'balance' => '4263',
--- app/mgrid_bkp/lib/Mgrid/Column/Filter/Render/ARender.php
+++ app/mgrid_bkp/lib/Mgrid/Column/Filter/Render/ARender.php
@@ -1,12 +1,32 @@
 <?php
 
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://mgrid.mdnsolutions.com/license>.
+ */
+
 namespace Mgrid\Column\Filter\Render;
 
 /**
- * Description of Text
+ * Number filter type
  *
- * @author Renato Medina <medinadato@gmail.com>
+ * @since       0.0.1
+ * @author      Renato Medina <medinadato@gmail.com>
  */
+
 abstract class ARender 
 {
     /**
@@ -114,6 +134,10 @@ abstract class ARender
         return $this;
     }
 
+    /**
+     * 
+     * @param type $attributes
+     */
     public function setAttributes($attributes)
     {
         //valores padroes para o Render
@@ -124,11 +148,38 @@ abstract class ARender
         $this->attributes = $attributes;
     }
     
+    /**
+     * 
+     * @param string $html
+     * @param array $attributes
+     * @return string
+     */
+    protected function generateHtmlOfAttributes($html = '', array $attributes = array())
+    {
+        foreach($attributes as $name => $value) {
+            if(is_array($value)) {
+                $value = implode(' ', $value);
+            }
+            
+            $html .= ' ' . $name . '="' . $value . '" ';
+        }
+        
+        return $html;
+    }
+    
+    /**
+     * 
+     * @return type
+     */
     public function getCondition()
     {
         return $this->condition;
     }
 
+    /**
+     * 
+     * @param type $condition
+     */
     public function setCondition($condition)
     {
         $this->condition = $condition;
